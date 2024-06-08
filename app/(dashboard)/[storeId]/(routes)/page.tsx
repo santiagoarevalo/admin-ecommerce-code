9811ce6b9e6fc1a441c1c5049c4b26bdd159e298
Santiago Arévalo <santiagoarevalo2003@gmail.com>
2024-06-08 00:01:36 -0500

    page.tsx modified

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -33,7 +33,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">
-                Total Revenue
+                Total Revenues
               </CardTitle>
               <DollarSign className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
